a526690e31b99a341c3dcd2793ea783494267a4b
VaheC <vcharchian@gmail.com>
2024-04-18 16:49:58 +0400

    utils code updated

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,3 +16,6 @@ def save_object(file_path, obj):
         pickle.dump(obj, open(file_path, 'wb'))
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    # create a function to return the results
